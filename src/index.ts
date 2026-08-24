b808923c4b578e3ff6738180b69397d83b0d70bb
Thribhu K <4tkbytes@pm.me>
2026-08-24 22:09:49 +1000

    refactor: cleaned up the env stuff, and moved the tag stuff to another module

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
+import 'dotenv/config'
+
 import { ActivityType, ChatInputCommandInteraction, Client, EmbedBuilder, Events, GatewayIntentBits, REST, Routes, SlashCommandBooleanOption, SlashCommandBuilder, SlashCommandStringOption } from 'discord.js'
 
-import { env } from '../env'
-import { allTags } from '@content'
+import { doTags } from "./tags"
+
+const { DISCORD_BOT_TOKEN, DISCORD_CLIENT_ID } = process.env
+if (!DISCORD_BOT_TOKEN || !DISCORD_CLIENT_ID) {
+    throw new Error('DISCORD_BOT_TOKEN and DISCORD_CLIENT_ID must be set')
+}
 
 const commands = [
     new SlashCommandBuilder()
@@ -22,38 +28,15 @@ const commands = [
 ]
 
 const commandMap: Record<string, (interaction: ChatInputCommandInteraction) => Promise<void>> = {
-    tags: async (interaction: ChatInputCommandInteraction) => {
-        const tag = allTags.find((v) => v._meta.fileName)
-        if (!tag) {
-            await interaction.reply({
-                content: 'I couldn\'t find a tag by that ID!',
-                flags: ["Ephemeral"]
-            })
-            return
-        }
-        const embed = new EmbedBuilder()
-            .setTitle(tag.title)
-            .setDescription(tag.content)
-            .setAuthor({
-                name: tag.author
-            })
-            .setFooter({
-                text: tag.footer ?? `Tag ID: ${tag._meta.fileName}`
-            })
-        await interaction.reply({
-            embeds: [embed],
-            flags: interaction.options.getBoolean('ephemeral', false) ? ["Ephemeral"] : []
-        })
-        return
-    }
+    tags: doTags
 }
 
-const rest = new REST({ version: '10' }).setToken(env.DISCORD_BOT_TOKEN)
+const rest = new REST({ version: '10' }).setToken(DISCORD_BOT_TOKEN)
 
 try {
   console.log('Started refreshing application (/) commands.');
 
-  await rest.put(Routes.applicationCommands(env.DISCORD_CLIENT_ID), { body: commands });
+  await rest.put(Routes.applicationCommands(DISCORD_CLIENT_ID), { body: commands });
 
   console.log('Successfully reloaded application (/) commands.');
 } catch (error) {
@@ -61,14 +44,14 @@ try {
 }
 
 const client = new Client({
-    intents: [GatewayIntentBits.MessageContent]
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages]
 })
 
 client.once(Events.ClientReady, (c) => {
     c.user.setPresence({
         activities: [
             {
-                name: `:robot: Version ${env.PUBLIC_BUILD_VERSION}`,
+                name: `:robot: Version ${process.env.PUBLIC_BUILD_VERSION}`,
                 type: ActivityType.Custom
             }
         ],
@@ -92,9 +75,11 @@ client.on(Events.InteractionCreate, async (m) => {
 })
 
 client.on(Events.MessageCreate, async (m) => {
-    if (m.channelId === '1541374516524220426') {
+    if (m.channelId === '1541374516524220426') { // what is this number??? put in dotenv plz
         await m.forward('1541235184450543646')
     }
 })
 
-await client.login(env.DISCORD_BOT_TOKEN)
+await client.login(DISCORD_BOT_TOKEN).then(()=>{
+    console.log("Ding! Fries are done!");
+})
